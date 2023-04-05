a70d4d00260b4339392d0a8c5ef86cc438939469
smith sese <samoa@javolin.com>
2023-04-05 08:47:41 +0000

    Initial Commit

---
+++ CoffeTestAPpUITests/CoffeTestAPpUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CoffeTestAPpUITestsLaunchTests.swift
+//  CoffeTestAPpUITests
+//
+//  Created by Smith Sese on 05/04/2023.
+//
+
+import XCTest
+
+final class CoffeTestAPpUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
